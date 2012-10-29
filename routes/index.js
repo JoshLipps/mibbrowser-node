00ed4f79fb0839ab12fc73ad21d34ad91832a24e
Josh Lipps <josh.lipps@gmail.com>
2012-10-28 21:04:56 -0700

    fixed header and footer, aside is broken

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,32 @@ var bodystuff,scripts;
 
 
 bodystuff =
-'<div class="inputbox-list well"><ul>\n'+
-' 	<li><label for="host">Host</label></li>\n'+
-'	<li><input class="input-small" type="text" id="host" name="host" value="snmp.yawnneko.com" /></li>\n'+
-'</ul>\n'+
-'<ul>\n'+
-'	<li><label for="community">Community</label></li>\n'+
-'	<li><input class="input-small" type="text" id="community" name="community" value="cs158b!" /></li>\n'+
-'</ul>\n'+
-'<ul>\n'+
-'	<li><label for="oid">OID</label></li>\n'+
-'	<li><input class="input-small" type="text" id="oid" name="oid" value=".1.3.6.1.2.1.1.3.0" /></li>\n'+
-'</ul>\n'+
-'<input class="btn" type="submit" value="Poll" onclick="toGetAPI()" />\n'+
+'<div class="inputbox-list well">'+
+'<div>\n'+
+' 	<label for="host">Host</label>\n'+
+'	<input class="input" type="text" id="host" name="host" value="snmp.yawnneko.com" />\n'+
 '</div>\n'+
-'<div><input type="file" id="files" name="file" />\n'+
+'<div>\n'+
+'	<label for="community">Community</label>\n'+
+'	<input class="input-small" type="text" id="community" name="community" value="cs158b!" />\n'+
+'</div>\n'+
+'<div>\n'+
+'	<label for="oid">OID</label>\n'+
+'	<input class="input" type="text" id="oid" name="oid" value=".1.3.6.1.2.1.1.3.0" />\n'+
+'</div>\n'+
+'<div>\n'+
+'	<label for="poll">Poll</label>\n'+
+'	<input class="btn" type="submit" value="Get" onclick="toGetAPI()" />\n'+
+'</div>\n'+
+'</div>\n'+
+'<div class="filebox well"><input type="file" id="files" name="file" />\n'+
 '	<div class="input-append">\n' +
 '		<input id="mibfile" class="input" type="text">\n'+
 '		<a class="btn" onclick="$(\'input[id=files]\').click();">Browse</a>\n'+
 '	</div>\n'+
-'</div>\n'+
-'<div><output id="list"> </output></div>';
+'	<div class="list"><output id="list"> </output></div>'+
+'</div>\n';
+;
 
 scripts =
 '<script src="/js/main.js"></script>\n' +
