517c6204c11df04770a505f099cf6302aec1e2d7
Josh Lipps <josh.lipps@gmail.com>
2012-10-28 21:13:13 -0700

    Aside removed and about page fixed

--- routes/about.js
+++ routes/about.js
@@ -5,22 +5,15 @@
 $ = require('jquery');
 
 var bodystuff =
+'<div class="hero-unit">\n'+
+'<h2>About</h2>\n'+
 '<p>We like MIBs and Node. Here\'s a list of tech used to generate this goodness:</p>\n'+
 '<ul>\n'+
 '<li>\n'+
 ["node.js","express","ejs","vows","initializr","GitHub","jQuery"].join('</li>\n<li>')+
 '</li>'+
-'</ul>';
+'</ul></div>';
 
-//var mylist = ['a','b','c'];
-
-//var list1234 = $('ul.alist');
-
-//$.each(mylist, function(index){
-//	$('<li/>').text(mylist[index]).appendTo($('.alist'));
-//});
-
-//console.log(list1234.html());
 
 var scripts = '';
 
