a0c7fcd3257e0372a44ec740e015c893f5dd71c3
Josh Lipps <josh.lipps@gmail.com>
2012-11-14 12:26:15 -0800

    added devices demo layout

--- routes/devices.js
+++ routes/devices.js
@@ -4,8 +4,25 @@
 // this should print all devices stored in the DB server and provide for adding new devices
 //
 
-var bodystuff =
-'';
+var bodystuff ='<div class="container-fluid">\n'+
+'  <div class="row-fluid">\n'+
+'    <div class="span3 well">\n'+
+'        <!--Sidebar content-->\n'+
+'        <ul class="nav nav-pills nav-stacked">\n'+
+'            <li class="active"><a href="devices/snmp">snmp.yawnneko.com</a></li>\n'+
+'            <li>loki.yawnneko.com</li>\n'+
+'            <li>router.yawnneko.com</li>\n'+
+'       </ul>\n'+
+'    </div>\n'+
+'    <div class="span9 well">\n'+
+'      <!--Body content-->\n'+
+'        Item 1<br>\n'+
+'        Item 2<br>\n'+
+'        Graph<br>\n'+
+'    </div>\n'+
+'  </div>\n'+
+'</div>';
+
 
 var scripts = '';
 
