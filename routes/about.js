66c88a039de8c9939999115a0ce4422d31bdf39a
Josh Lipps <josh.lipps@gmail.com>
2012-12-08 18:12:26 -0800

    commed some missing css

--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,7 @@
 //
 //$ = require('jquery');
 
-var bodystuff = '',scripts = '',css = '<link rel="stylesheet" href="css/about.css" />';
+var bodystuff = '',scripts = '',css = '';
 
 
 exports.index = function(req, res){
