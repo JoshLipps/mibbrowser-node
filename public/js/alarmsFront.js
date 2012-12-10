6b485b6cc4505221f13a1fdfac03e22be4a13570
Josh Lipps <josh.lipps@gmail.com>
2012-12-10 09:50:41 -0800

    fixed modal layout(mostly)

--- public/js/alarmsFront.js
+++ public/js/alarmsFront.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
    //$('div.thedialog').dialog({ autoOpen: false })
    $('.icon-wrench').click(function(){$("#myModal").modal('show');});
+   fillModal("thor.yawnneko.com");
 });
 
 function activeclick(elem){
@@ -16,7 +17,6 @@ function activeclick(elem){
 }
 function redrawAlarms(id){
 	$("#alarms").children().remove();
-	$().get();
 	$.get('/api/Events',{device:id}, function(data){
     	for(var i=0;i<data.length;i++){
     		$("#alarms").append('<tr class="'+data[i].state+'" >'+
@@ -30,8 +30,38 @@ function redrawAlarms(id){
     });
 }
 function fillModal(id){
-	
+	$("#oids").children().remove();
+	$.get('api/Host',{hostname:id},function(data){
+		$('#hostname').val(data.hostname);
+		$('#community').val(data.community);
+		$('#oid').val(data.oid);
+		$('#port').val(data.port);
+		for(var j=0;j<data.alarms.length;j++){
+			var alarm = data.alarms[j];
+			$("#oids").append('<tr >'+
+				'<td><input class="input-small" type="text" value='+alarm.oid+'></td>'+
+				'<td><input class="input-small" type="text" value='+alarm.error+'></td>'+
+				'<td><input class="input-small" type="text" value='+alarm.errormsg+'></td>'+
+				'<td><input class="input-small" type="text" value='+alarm.warn+'></td>'+
+				'<td><input class="input-small" type="text" value='+alarm.warnmsg+'></td>'+
+				'<td><input class="input-small" type="text" value='+alarm.clear+'></td>'+
+				'<td><input class="input-small" type="text" value='+alarm.clearmsg+'></td>'+
+				'</tr>');
+		}
+		$("#oids").append('<tr>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'<td><input class="input-small" type="text"></td>'+
+				'</tr>');
+
+		//<td id="oid"><input type="text" id="" placeholder="yo"><td>
+	});
 }
+
 
 function jq(myid) {
    return '#' + myid.replace(/(:|\.)/g,'\\$1');
